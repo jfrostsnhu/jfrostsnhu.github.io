2460481f04d270beb15ef137b4edbd15d9719253
jfrostsnhu <james.frost3@snhu.edu>
2025-02-18 10:07:12 -0500

    Corrected bug with Code Review expand/collapse function

--- assets/js/ePortfolioJS.js
+++ assets/js/ePortfolioJS.js
@@ -391,7 +391,8 @@ document.addEventListener('DOMContentLoaded', function() {
                     'enh2Collapse', 'bp2Collapse', 'cr2Collapse',
                     'efgCollapse', 'ar1Collapse', 'enh1Collapse',
                     'bp1Collapse', 'cr1Collapse', 'flcCollapse',
-                  'refCollapse', 'skillsCollapse', 'planCollapse']
+                    'refCollapse','croCollapse', 'acrCollapse',
+                    'skillsCollapse', 'planCollapse']
   const expandIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" class="icon-git-light mr-2" viewBox="0 0 16 16">
           <path fill-rule="evenodd" d="M3.646 10.146a.5.5 0 0 1 .708 0L8 13.793l3.646-3.647a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 0-.708m0-4.292a.5.5 0 0 0 .708 0L8 2.207l3.646 3.647a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 0 0 0 .708M1 8a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13A.5.5 0 0 1 1 8"/>
           </svg>`;
